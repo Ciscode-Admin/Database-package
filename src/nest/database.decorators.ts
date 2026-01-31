c352788b5222cc745531fcab02a378a15cd5ad03
Zaiidmo <zaiidmoumnii@gmail.com>
2026-01-31 20:53:57 +0100

    refactor: remove old core/ and nest/ directory structure
    - Deleted src/core/adapters/mongo.adapter.ts
    - Deleted src/core/adapters/postgres.adapter.ts
    - Deleted src/core/contracts.ts
    - Deleted src/core/database.ts
    - Deleted src/nest/database.decorators.ts
    - Deleted src/nest/database.module.ts
    
    BREAKING CHANGE: Complete restructure to new architecture

--- src/nest/database.decorators.ts
+++
@@ -1,5 +0,0 @@
-// src/nest/database.decorators.ts
-import { Inject } from '@nestjs/common';
-import { DATABASE_TOKEN } from './database.module';
-
-export const InjectDatabase = () => Inject(DATABASE_TOKEN);
